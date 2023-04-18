37d4ce25a182bdbb25b9347d1f26625a326ec8f2
wieng24 <wosee24@gmail.com>
2023-04-18 23:31:51 +0700

    tugaswp3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Fiqri</li>
+ <li>Nama Belakang : Dwi Prasetya</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="disc">
+ <li>Jalan Percetakan Negara 1 No. 10</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="disc">
+ <li>Jakarta Keras</li>
+ </ul>
+ <h4>Hobi Favorit</h4>
+ <ul type="square">
+ <li>Rebahan ajah</li>
+ <li>Gaming</li>
+ </ul>
+</section>
